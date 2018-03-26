addef29a519c490b8802b018ad3a921725e596c0
tiff-wang <wangtiffany@gmail.com>
2018-03-26 00:47:37 -0400

    can upload mp3 files

--- Android/app/src/main/java/microp/android/MainActivity.java
+++ Android/app/src/main/java/microp/android/MainActivity.java
@@ -14,7 +14,11 @@ public class MainActivity extends AppCompatActivity {
     FirebaseStorage storage = FirebaseStorage.getInstance();
     // Create a storage reference from our app
     StorageReference storageRef = storage.getReference();
-    StorageReference ourFileRef = storageRef.child("boardData/testfile.txt");
+    StorageReference ourFileRef = storageRef.child("boardData/testfile.mp3");
+    // Create file metadata including the content type
+    StorageMetadata metadata = new StorageMetadata.Builder()
+            .setContentType("audio/mpeg")
+            .build();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +33,11 @@ public class MainActivity extends AppCompatActivity {
 
     void uploadResource() throws IOException {
 // the text we will upload
-        String someTextToUpload = "Hello world! \nthis is to test uploading data";
+        String someTextToUpload = "Hellghggfhgfghfgfho world! \nthis is to test uploading data";
 //convert the text to bytes
         byte[] file = someTextToUpload.getBytes();
 // Now we need to use the UploadTask class to upload to our cloud
-        UploadTask uploadTask = ourFileRef.putBytes(file);
+        UploadTask uploadTask = ourFileRef.putBytes(file, metadata);
         uploadTask.addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception exception) {
