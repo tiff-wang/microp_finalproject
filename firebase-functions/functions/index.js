99c5422e71fb46b8ad24bd89dfb57d9ccbb44c5a
tiff-wang <wangtiffany@gmail.com>
2018-03-29 18:02:33 -0400

    changed to wav files - firebase voice reconigtion should work without the timeout error

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -1,13 +1,12 @@
 'use strict';
 
 const functions = require('firebase-functions');
-const mkdirp = require('mkdirp-promise');
 const gcs = require('@google-cloud/storage')();
-const speech = require('@google-cloud/speech');
-const spawn = require('child-process-promise').spawn;
 const path = require('path');
 const os = require('os');
 const fs = require('fs');
+const https = require('https')
+const { BingSpeechClient, VoiceRecognitionResponse } = require('bingspeech-api-client');
 
 
 /**
@@ -37,93 +36,53 @@ exports.number = functions.storage.object().onChange((event) => {
     return null;
   }
 
-  return test(object.name, object.bucket, object.metadata);
+  return recognize(object.name, object.bucket, object.metadata);
 });
 
 
 function recognize(filePath, bucketName, metadata) {
-  const tempLocalFile = path.join(os.tmpdir(), filePath);
-  const tempLocalDir = path.dirname(tempLocalFile);
-  const bucket = gcs.bucket(bucketName);
-  const client = new speech.SpeechClient();
+  // let downloadUrl = "https://firebasestorage.googleapis.com/v0/b/" + bucketName + "/o/" + filePath + "?alt=media&token=" + metadata.firebaseStorageDownloadTokens
 
-  const config = {
-    encoding: 'LINEAR16',
-    sampleRateHertz: '16000',
-    languageCode: 'en-US',
-  };
-  const audio = {
-    uri: "gs://" + bucketName + "/" + filePath,
-  };
+  let downloadUrl = "https://firebasestorage.googleapis.com/v0/b/microp-g2.appspot.com/o/test-3.wav?alt=media&token=b271de9f-a8dc-4e0f-b9f9-e6cb31320c59"
 
-  const request = {
-    config: config,
-    audio: audio,
-  };
-  // Create the temp directory where the storage file will be downloaded.
-  return mkdirp(tempLocalDir).then(() => {
-    console.log('Temporary directory has been created', tempLocalDir);
-    // Download file from bucket.
-    return bucket.file(filePath).download({destination: tempLocalFile});
-  }).then(() => {
-    console.log('The file has been downloaded to', tempLocalFile);
-    // Uploading the Blurred image.
-    filePath = 'proc_' + filePath;
-    return bucket.upload(tempLocalFile, {
-      destination: filePath,
-      metadata: {metadata: metadata}, // Keeping custom metadata.
-    });
-  }).then(() => {
-    filePath = 'proc_' + filePath;
-    console.log('Image uploaded to Storage at', filePath);
-    fs.unlinkSync(tempLocalFile);
-    return console.log('Deleted local file', filePath);
+  console.log(downloadUrl)
+  var request = https.get("https://firebasestorage.googleapis.com/v0/b/microp-g2.appspot.com/o/test-3.wav?alt=media&token=b271de9f-a8dc-4e0f-b9f9-e6cb31320c59", (response) => {
+    console.log("hiiiiiiiiiii")
+    console.log(response)
+    let subscriptionKey = '062461c0e7f5456abcc4791b559907f2';
+    let client = new BingSpeechClient(subscriptionKey);
+    client.recognizeStream(response).then(response => {
+      return console.log(response.results[0].name)
+    }).catch(err => 
+      console.log(err)
+    );
   });
-}
 
-function test(filePath, bucketName, metadata){
-	const speech = require('@google-cloud/speech');
-	const fs = require('fs');
 
-	// Creates a client
-	const client = new speech.SpeechClient();
 
-	// The name of the audio file to transcribe
-	const fileName = './resources/audio.raw';
+  // const tempLocalFile = path.join(os.tmpdir(), filePath);
+  // const tempLocalDir = path.dirname(tempLocalFile);
+  // const bucket = gcs.bucket(bucketName);
+  // const client = new speech.SpeechClient();
 
-	// Reads a local audio file and converts it to base64
-	const file = fs.readFileSync(fileName);
-	const audioBytes = file.toString('base64');
-
-	// The audio file's encoding, sample rate in hertz, and BCP-47 language code
-	const audio = {
-	  "uri": "gs://" + bucketName + "/" + filePath
-	};
-	const config = {
-    "encoding": "LINEAR16",
-    "sampleRateHertz": 16000,
-    "languageCode": "en-US"
-	};
-	const request = {
-	  audio: audio,
-	  config: config,
-	};
-
-	// Detects speech in the audio file
-	client
-	  .recognize(request)
-	  .then(data => {
-	    const response = data[0];
-	    const transcription = response.results
-	      .map(result => result.alternatives[0].transcript)
-	      .join('\n');
-	    console.log(`Transcription: ${transcription}`);
-
-	    return transcription
-	  })
-	  .catch(err => {
-	    console.error('ERROR:', err);
-	  });
-
-	return null;
+  
+  // Create the temp directory where the storage file will be downloaded.
+  // return mkdirp(tempLocalDir).then(() => {
+  //   console.log('Temporary directory has been created', tempLocalDir);
+  //   // Download file from bucket.
+  //   return bucket.file(filePath).download({destination: tempLocalFile});
+  // }).then(() => {
+  //   console.log('The file has been downloaded to', tempLocalFile);
+  //   // Uploading the Blurred image.
+  //   filePath = 'proc_' + filePath;
+  //   return bucket.upload(tempLocalFile, {
+  //     destination: filePath,
+  //     metadata: {metadata: metadata}, // Keeping custom metadata.
+  //   });
+  // }).then(() => {
+  //   filePath = 'proc_' + filePath;
+  //   console.log('Image uploaded to Storage at', filePath);
+  //   fs.unlinkSync(tempLocalFile);
+  //   return console.log('Deleted local file', filePath);
+  // });
 }
